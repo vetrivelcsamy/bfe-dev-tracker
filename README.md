1cf741a9479f912589efd92f4d3908c1402c4075
Vetrivel Chinnasamy <vetrivelcsamy@pop-os.localdomain>
2023-12-06 20:06:54 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+<h1 align="center">
+  <img src="assets/logo2.png" style="width: 10%;" />
+  <br>
+    <B>P</B>ractice <B>M</B>akes <B>C</B>ode <B>A</B>ccepted
+  <br>
+</h1>
+
+
+<h2 align="center">
+     Review BFE.dev / bigfrontend.dev problems based on Ebbinghaus's Forgetting Curve
+</h2>
+
+<br>
+
+# Get Started
+
+1. Install the PMCA extension in your web browser.
+2. Solve and submit BFE.dev / bigfrontend.dev problems. Upon successful submission, BFE.dev tracker sets the next review time for the problem based on the Ebbinghaus forgetting curve.
+3. Open BFE.dev tracker to view problems that need to be reviewed now, problems scheduled for future review and mastered problems. You can use the switch in the popup page to switch between the LeetCode global site and the LeetCode CN site.
+4. Enjoy coding!
+
+_Currently BFE.dev tracker __does not__ support tracking problem submission by `ctrl` + `Enter` Please refrain from using it now, thanks! (I will try to fix it soon (or not 🤡_. 
---
+++ scripts/bfe.js
@@ -0,0 +1,195 @@
+// Problem object
+class Problem {
+    constructor(index, name, level, url, submissionTime, proficiency) {
+        this.index = index;
+        this.name = name;
+        this.level = level;
+        this.url = url;
+        this.submissionTime = submissionTime;
+        this.proficiency = proficiency;
+    }
+};
+
+
+// Ebbinghaus utils
+const forggetingCurve = [
+    1 * 24 * 60,    // 1 day
+    2 * 24 * 60,    // 2 day
+    4 * 24 * 60,    // 4 day
+    7 * 24 * 60,    // 7 day
+    15 * 24 * 60    // 15 day
+];
+
+
+
+const needReview = (problem) => {
+    if (problem.proficiency >= forggetingCurve.length) {
+        return false;
+    }
+
+    const currentTime = Date.now();
+    const timeDiffInMinute = (currentTime - problem.submissionTime) / (1000 * 60);
+    return timeDiffInMinute >= forggetingCurve[problem.proficiency];
+};
+
+
+
+
+/* Main Body */
+
+/*  
+    Read User Configguration
+*/
+
+let easy_proficiency_steps = [1, 3];
+let medium_proficiency_steps = [1, 3, 4];
+let hard_proficiency_steps = [0, 1, 2, 3, 4];
+
+const readConfig = () => {
+    chrome.storage.local.get('config', (config) => {
+        if (config !== undefined) {
+            ({ easy_proficiency_steps, medium_proficiency_steps, hard_proficiency_steps } = config);
+        }
+    })
+}
+
+
+const getDifficultyBasedSteps = (diffculty) => {
+    if (diffculty === "Easy" || diffculty === "easy") {
+        return easy_proficiency_steps;
+    } else if (diffculty === "Medium" || diffculty === "medium") {
+        return medium_proficiency_steps;
+    } else {
+        return hard_proficiency_steps;
+    }
+}
+
+/*
+    Extract basic problem information
+*/
+const extractProblemInfo = async () => {
+
+    const QUESTION_INFO = document.getElementById('__NEXT_DATA__').innerText;
+
+    return {
+        problemIndex: JSON.parse(QUESTION_INFO)['props']['pageProps']['item']['id'],
+        problemName: JSON.parse(QUESTION_INFO)['props']['pageProps']['item']['title'],
+        problemLevel: JSON.parse(QUESTION_INFO)['props']['pageProps']['item']['difficulty'],
+        problemUrl: `https://bigfrontend.dev/problem/${JSON.parse(QUESTION_INFO)['query']['permalink']}`
+    };
+}
+
+
+/* 
+    monitorSubmissionResult will repeateadly check for the submission result.
+*/
+const monitorSubmissionResult = () => {
+    console.log("monitorSubmissionResult: submitted!")
+
+    let submissionResult;
+    let maxRetry = 10;
+    const retryInterval = 1000;
+
+    const functionId = setInterval(async () => {
+
+        if (maxRetry <= 0) {
+            clearInterval(functionId);
+            return;
+        }
+
+        submissionResult = document.querySelector("#__next > div.Page__Outer-sc-90ozb8-0.cCuUVy > main > div.ProblemDetailJS__Judge-ffiw7z-3.dxhNjB > div.ProblemDetailJS__JudgeFooter-ffiw7z-5.enghjr > div.ProblemDetailJS__Logs-ffiw7z-6.ezuvBO > p.TestResultView__Success-sc-20rtgt-0.fzUVDA");
+
+
+        if (!submissionResult) {
+            maxRetry--;
+            return;
+        }
+
+        clearInterval(functionId);
+        let isSuccess = submissionResult.innerText.includes('👍 well done! all cases passed');
+
+        const submissionTime = Date.now();
+        const { problemIndex, problemName, problemLevel, problemUrl } = await extractProblemInfo();
+
+        console.log(problemIndex, "-------");
+
+        const steps = getDifficultyBasedSteps(problemLevel);
+
+        const promise = new Promise((resolve, reject) => {
+            chrome.storage.local.get("bfe_records", (result) => {
+                const problems = result.records;
+                if (problems === undefined || problems[problemIndex] === undefined) {
+                    reject(problems);
+                } else {
+                    resolve(problems);
+                }
+            })
+        })
+
+        promise.then(
+            // problem submitted before
+            problems => {
+                const problem = problems[problemIndex];
+                const reviewNeeded = needReview(problem);
+                if (reviewNeeded && isSuccess) {
+                    let nextProficiencyIndex;
+                    for (const i of steps) {
+                        if (i > problem.proficiency) {
+                            nextProficiencyIndex = i;
+                            break;
+                        }
+                    }
+
+                    // further review needed
+                    if (nextProficiencyIndex !== undefined) {
+                        problem.proficiency = nextProficiencyIndex;
+                        // already completed all review
+                    } else {
+                        problem.proficiency = forggetingCurve.length;
+                    }
+
+                    problem.submissionTime = submissionTime;
+                    monitorSubmissionResult
+                    problems[problemIndex] = problem;
+                    chrome.storage.local.set({ "bfe_records": problems });
+                }
+            },
+            // first time submission
+            problems => {
+                if (isSuccess) {
+                    if (problems === undefined) {
+                        problems = {};
+                    }
+
+                    const problem = new Problem(problemIndex, problemName, problemLevel, problemUrl, submissionTime, steps[0]);
+
+                    problems[problemIndex] = problem;
+
+                    chrome.storage.local.set({ "bfe_records": problems });
+                }
+            }
+        ).finally(() => console.log("Submission successfully tracked!"));
+
+
+    }, retryInterval)
+};
+
+
+/*
+    Invoke monitorSubmissionResult upon clicking the submit button.
+*/
+
+const isSubmitButton = (element) => {
+    return element.getAttribute(SUBMIT_BUTTON_ATTRIBUTE_NAME) === SUBMIT_BUTTON_ATTRIBUTE_VALUE;
+}
+
+document.addEventListener('click', (event) => {
+    const element = event.target;
+
+    if (element.innerText.includes('submit')) {
+        monitorSubmissionResult();
+        console.log("Submisson submitted!")
+    }
+
+});
+
